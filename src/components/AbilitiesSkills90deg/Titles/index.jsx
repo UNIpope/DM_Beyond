41fdc5ba1e3eba61ff014e83cce31afbe13c88b3
UNIpope <jack.dugg.ch@gmail.com>
2021-07-06 17:47:52 +0200

    90 as

--- src/components/AbilitiesSkills90deg/Titles/index.jsx
+++ src/components/AbilitiesSkills90deg/Titles/index.jsx
@@ -6,50 +6,50 @@ import "./titles.css"
 class AS90titles extends React.Component {
   render() {
     return (
-    <div className="AStitle-container">
+    <div className="AS90title-container">
         <Background/>
-        <p className="AStitle-inspiration AStitle-titles">Inspiration</p>
-        <p className="AStitle-pbonus AStitle-titles">Prof. Bonus</p>
-        <p className="AStitle-perception AStitle-titles">Perception</p>
+        <p className="AS90title-inspiration AS90title-titles">Inspiration</p>
+        <p className="AS90title-pbonus AS90title-titles">Prof. Bonus</p>
+        <p className="AS90title-perception AS90title-titles">Perception</p>
         
-        <p className="AStitle-str AStitle-titles">Strength</p>
-        <p className="AStitle-strsave AStitle-titles">Saving Throw</p>
-        <p className="AStitle-athletics AStitle-titles">Athletics</p>
+        <p className="AS90title-str AS90title-titles">Strength</p>
+        <p className="AS90title-strsave AS90title-titles">Saving Throw</p>
+        <p className="AS90title-athletics AS90title-titles">Athletics</p>
         
-        <p className="AStitle-dex AStitle-titles">Dexterity</p>
-        <p className="AStitle-dexsave AStitle-titles">Saving Throw</p>
-        <p className="AStitle-acrobatics AStitle-titles">Acrobatics</p>
-        <p className="AStitle-sofhand AStitle-titles">Sleight of Hand</p>
-        <p className="AStitle-stealth AStitle-titles">Stealth</p>
-        <p className="AStitle-rope AStitle-titles">Use Rope</p>
+        <p className="AS90title-dex AS90title-titles">Dexterity</p>
+        <p className="AS90title-dexsave AS90title-titles">Saving Throw</p>
+        <p className="AS90title-acrobatics AS90title-titles">Acrobatics</p>
+        <p className="AS90title-sofhand AS90title-titles">Sleight of Hand</p>
+        <p className="AS90title-stealth AS90title-titles">Stealth</p>
+        <p className="AS90title-rope AS90title-titles">Use Rope</p>
 
-        <p className="AStitle-con AStitle-titles">Constitution</p>
-        <p className="AStitle-consave AStitle-titles">Saving Throw</p>
-        <p className="AStitle-brewing AStitle-titles">Brewing</p>
+        <p className="AS90title-con AS90title-titles">Constitution</p>
+        <p className="AS90title-consave AS90title-titles">Saving Throw</p>
+        <p className="AS90title-brewing AS90title-titles">Brewing</p>
 
-        <p className="AStitle-int AStitle-titles">Intelligence</p>
-        <p className="AStitle-intsave AStitle-titles">Saving Throw</p>
-        <p className="AStitle-arcana AStitle-titles">Arcana</p>
-        <p className="AStitle-history AStitle-titles">Lore</p>
-        <p className="AStitle-invest AStitle-titles">Investigation</p>
-        <p className="AStitle-nature AStitle-titles">Nature</p>
-        <p className="AStitle-religion AStitle-titles">Religion</p>
-        <p className="AStitle-appraisal AStitle-titles">Appraisal</p>
+        <p className="AS90title-int AS90title-titles">Intelligence</p>
+        <p className="AS90title-intsave AS90title-titles">Saving Throw</p>
+        <p className="AS90title-arcana AS90title-titles">Arcana</p>
+        <p className="AS90title-history AS90title-titles">Lore</p>
+        <p className="AS90title-invest AS90title-titles">Investigation</p>
+        <p className="AS90title-nature AS90title-titles">Nature</p>
+        <p className="AS90title-religion AS90title-titles">Religion</p>
+        <p className="AS90title-appraisal AS90title-titles">Appraisal</p>
 
-        <p className="AStitle-wis AStitle-titles">Wisdom</p>
-        <p className="AStitle-wissave AStitle-titles">Saving Throw</p>
-        <p className="AStitle-ahand AStitle-titles">Animal Handling</p>
-        <p className="AStitle-insight AStitle-titles">Insight</p>
-        <p className="AStitle-medicine AStitle-titles">Medicine</p>
-        <p className="AStitle-percept AStitle-titles">Perception</p>
-        <p className="AStitle-survival AStitle-titles">Survival</p>
+        <p className="AS90title-wis AS90title-titles">Wisdom</p>
+        <p className="AS90title-wissave AS90title-titles">Saving Throw</p>
+        <p className="AS90title-ahand AS90title-titles">Animal Handling</p>
+        <p className="AS90title-insight AS90title-titles">Insight</p>
+        <p className="AS90title-medicine AS90title-titles">Medicine</p>
+        <p className="AS90title-percept AS90title-titles">Perception</p>
+        <p className="AS90title-survival AS90title-titles">Survival</p>
 
-        <p className="AStitle-cha AStitle-titles">charisma</p>
-        <p className="AStitle-chasave AStitle-titles">Saving Throw</p>
-        <p className="AStitle-deception AStitle-titles">Deception</p>
-        <p className="AStitle-intimidate AStitle-titles">Intimidation</p>
-        <p className="AStitle-performance AStitle-titles">Performance</p>
-        <p className="AStitle-persuasion AStitle-titles">Persuasion</p>
+        <p className="AS90title-cha AS90title-titles">charisma</p>
+        <p className="AS90title-chasave AS90title-titles">Saving Throw</p>
+        <p className="AS90title-deception AS90title-titles">Deception</p>
+        <p className="AS90title-intimidate AS90title-titles">Intimidation</p>
+        <p className="AS90title-performance AS90title-titles">Performance</p>
+        <p className="AS90title-persuasion AS90title-titles">Persuasion</p>
     </div>
     );
   }
